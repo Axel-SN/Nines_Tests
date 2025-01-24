e92733482a78ca077a2fc4cca65f7dcf1586c300
Axel-SN <137889141+Axel-SN@users.noreply.github.com>
2025-01-24 18:23:50 +0100

    Implemented prizing structure as well as functionalities and outputs to visualize them
    Added prizing into the nines project.
    For prizing, functions to calculate the prize for a given value were added.
    Calculations for each releveant three-value set (row, column, diagonal) were added to showcase the prize in case any of the given sets would be chose.
    
    Added small comments to each function and block of code to describe its functionality lightly.
    
    Refactored some parts of the code to eliminate repetitive pieces of code by using loops and adding functions.
    
    Added more datastructures to interact with the possible three-value sets.

--- Nines_Basics.js
+++ Nines_Basics.js
@@ -1,3 +1,5 @@
+// Test functions from the first attempts at creating a randomized array, kept here in case they are useful for now
+
 function arrayCheck(array, number) {
   let breakpoint = 0;
   for (let i = 0; i < array.length; i++) {
@@ -25,6 +27,7 @@ function addNewRandomNumber(array, number) {
 
 //
 
+// A function to implement the Fisher–Yates shuffle for arrays - code snippet found online
 function shuffleArray(array) {
   for (let i = array.length - 1; i >= 1; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -33,12 +36,14 @@ function shuffleArray(array) {
   return array;
 }
 
+// A funtion to output an array containing nine elements in a three by three format to showcase rows and columns better
 function threebythree(array) {
   for (let i = 0; i < 8; i = i + 3) {
     console.log(array[i] + " - " + array[i + 1] + " - " + array[i + 2]);
   }
 }
 
+// A function that calculates the cross sum of a given set of numbers as an array
 function crossSum(array) {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
@@ -47,55 +52,71 @@ function crossSum(array) {
   return sum;
 }
 
+// A function that uses a passed value of a cross sum and returns the given element index of the prizes array to be used
+function prizeSelect(prizes, value) {
+  for (let b = 0; b < prizes.length; b++) {
+    if (prizes[b].sum == value) {
+      return b;
+    }
+  }
+  return 0;
+}
+
+// variables for quick testing purposes
 const x = 9;
 let check = 0;
+const pv = "Prize if you chose ";
 
+// an array to showcase the prizes available for each cross sum
+const prizes = [
+  { sum: 6, prize: 10000 },
+  { sum: 7, prize: 36 },
+  { sum: 8, prize: 720 },
+  { sum: 9, prize: 360 },
+  { sum: 10, prize: 80 },
+  { sum: 11, prize: 252 },
+  { sum: 12, prize: 108 },
+  { sum: 13, prize: 72 },
+  { sum: 14, prize: 54 },
+  { sum: 15, prize: 180 },
+  { sum: 16, prize: 72 },
+  { sum: 17, prize: 180 },
+  { sum: 18, prize: 119 },
+  { sum: 19, prize: 36 },
+  { sum: 20, prize: 306 },
+  { sum: 21, prize: 1080 },
+  { sum: 22, prize: 144 },
+  { sum: 23, prize: 1800 },
+  { sum: 24, prize: 3600 },
+];
+
+// create a basic array of 9 numbers, then create a shuffled array for each instance of a nines game
 const ninesClean = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const nines = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//let shuffledNines = nines.sort((a, b) => 0.5 - Math.random());
 const shuffledNines = shuffleArray(nines);
 
+// define the structures for rows, columns and diagonals in a given set of nines by creating multiple arrays
 const row1 = [shuffledNines[0], shuffledNines[1], shuffledNines[2]];
 const row2 = [shuffledNines[3], shuffledNines[4], shuffledNines[5]];
 const row3 = [shuffledNines[6], shuffledNines[7], shuffledNines[8]];
+const rows = [row1, row2, row3];
 const column1 = [shuffledNines[0], shuffledNines[3], shuffledNines[6]];
 const column2 = [shuffledNines[1], shuffledNines[4], shuffledNines[7]];
 const column3 = [shuffledNines[2], shuffledNines[5], shuffledNines[8]];
+const columns = [column1, column2, column3];
 const diagonalLeft = [shuffledNines[0], shuffledNines[4], shuffledNines[8]];
 const diagonalRight = [shuffledNines[2], shuffledNines[4], shuffledNines[6]];
+const diagonals = [diagonalLeft, diagonalRight];
 
-/*for (let i = 0; i < x; i++) {
-  check = Math.floor(Math.random() * 9) + 1;
-  if (i > 0) {
-    nines[i] = addNewRandomNumber(nines, check);
-  } else {
-    nines[i] = check;
-  }
-
-  //console.log(nines);
-
-  console.log("Nines Array at Position [" + i + "]: " + nines[i] + " !");
-  if (arrayCheck(nines, check)) {
-    console.log("But we already have this in our array.");
-  }
-}
-*/
-
+// output the base array and the shuffled array
 console.log("Base Array: " + ninesClean);
 console.log("Shuffled Array: " + shuffledNines);
 
-/*console.log(
-  shuffledNines[0] + " - " + shuffledNines[1] + " - " + shuffledNines[2]
-);
-console.log(
-  shuffledNines[3] + " - " + shuffledNines[4] + " - " + shuffledNines[5]
-);
-console.log(
-  shuffledNines[6] + " - " + shuffledNines[7] + " - " + shuffledNines[8]
-);
-*/
+// output the array in a three be three row/column format for better visualization
 console.log("Three by three:");
 threebythree(shuffledNines);
+
+// calculate and output the cross sum of each row, column and diagonal
 console.log("Cross sum of row1: " + crossSum(row1));
 console.log("Cross sum of row2: " + crossSum(row2));
 console.log("Cross sum of row3: " + crossSum(row3));
@@ -108,3 +129,39 @@ console.log(
 console.log(
   "Cross sum of diagonal Upper right to lower left: " + crossSum(diagonalRight)
 );
+
+// Show the prize of each cross sum for the given set of nines
+
+for (let r = 0; r < rows.length; r++) {
+  console.log(
+    pv +
+      "row" +
+      (r + 1) +
+      " = " +
+      crossSum(rows[r]) +
+      ": " +
+      prizes[prizeSelect(prizes, crossSum(rows[r]))].prize
+  );
+}
+for (let c = 0; c < columns.length; c++) {
+  console.log(
+    pv +
+      "column" +
+      (c + 1) +
+      " = " +
+      crossSum(rows[c]) +
+      ": " +
+      prizes[prizeSelect(prizes, crossSum(columns[c]))].prize
+  );
+}
+for (let d = 0; d < diagonals.length; d++) {
+  console.log(
+    pv +
+      "diagonal" +
+      (d + 1) +
+      " = " +
+      crossSum(diagonals[d]) +
+      ": " +
+      prizes[prizeSelect(prizes, crossSum(diagonals[d]))].prize
+  );
+}
